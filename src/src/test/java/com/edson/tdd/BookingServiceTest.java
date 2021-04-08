3a8f8e2929c2bba7d8279e2dc861033b09f3227a
Edson Ferreira <edson.da.silva@avanade.com>
2021-04-08 14:13:45 -0300

    passed test

--- src/src/test/java/com/edson/tdd/BookingServiceTest.java
+++ src/src/test/java/com/edson/tdd/BookingServiceTest.java
@@ -1,8 +1,55 @@
 package com.edson.tdd;
 
+import com.edson.tdd.model.BookingModel;
+import com.edson.tdd.repository.BookingRepository;
+import com.edson.tdd.service.BookingService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
 
 @ExtendWith(SpringExtension.class)
 public class BookingServiceTest {
+
+    @TestConfiguration
+    static class BookingServiceTestConfiguration{
+
+        @Bean
+        public BookingService bookingService(){
+            return new BookingService();
+        }
+    }
+
+    @Autowired
+    BookingService bookingService;
+
+    @MockBean
+    BookingRepository bookingRepository;
+
+    @Test
+    public void bookingTestServiceDaysCalculator(){
+        String name = "Edson";
+        int days = bookingService.daysCalculatorWithDatabase(name);
+
+        Assertions.assertEquals(days, 10);
+
+    }
+
+    @BeforeEach
+    public void setup(){
+        LocalDate checkIn = LocalDate.parse("2021-03-29");
+        LocalDate checkOut = LocalDate.parse("2021-04-08");
+        BookingModel bookingModel = new BookingModel("1", "Edson", checkIn, checkOut, 2);
+
+        Mockito.when(bookingRepository.findByReserveName(bookingModel.getReserveName()))
+                .thenReturn(java.util.Optional.of(bookingModel));
+    }
 }
